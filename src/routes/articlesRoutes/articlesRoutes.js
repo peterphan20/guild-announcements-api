36e0d5b765b81145b23453f0b46148d149de06b4
Peter Phan <peterphan0422@gmail.com>
2021-08-19 23:38:17 -0500

    feat: changed get article route to not require authentication

--- src/routes/articlesRoutes/articlesRoutes.js
+++ src/routes/articlesRoutes/articlesRoutes.js
@@ -19,4 +19,37 @@ module.exports = async function articleRoutes(fastify) {
     client.release()
     return { code: 200, rows }
   })
+
+  fastify.get('/articles/:id', async request => {
+    const { id } = request.params
+    const client = await fastify.pg.connect()
+    const { rows } = await client.query(
+      `
+      SELECT 
+        a.title, 
+        a.content, 
+        a.img_url, 
+        a.video_url,
+        json_agg(json_build_object(
+          'commentContent', c.content,
+          'createdAt', c.created_at,
+          'username', u.username
+        )) AS comments
+      FROM articles a
+      LEFT JOIN comments c 
+        ON c.article_id = a.article_id
+      LEFT JOIN users u
+        ON u.id = c.author_id
+      WHERE a.article_id=$1
+      GROUP BY 
+        a.title,
+        a.content,
+        a.img_url,
+        a.video_url;
+      `,
+      [id]
+    )
+    client.release()
+    return { code: 200, rows }
+  })
 }
